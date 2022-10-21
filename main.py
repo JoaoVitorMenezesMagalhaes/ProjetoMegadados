cd7f9fe2dd73e7d3967ca616af08fd2e287c70dc
JoaoVitorMenezesMagalhaes <joaovmm@al.insper.edu.br>
2022-10-20 21:39:28 -0300

    adicionando o post

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from typing import Union
 
+from tomlkit import item
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -8,12 +10,16 @@ app = FastAPI()
 class Item(BaseModel):
     name: str
     price: float
-    is_offer: Union[bool, None] = None
+
+@app.post("/items/{item_id}")
+def create_item(item_id: int, item: Item):
+    return {item_id, item}
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int):
+    return {"item_id": item_id}
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id, "item_price": item.price}
 
-@app.get("/items/{item_id}")
-def get_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
